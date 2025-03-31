35534e3e76dca1ae646ee0cd571a74b18bf2f01a
Jeffer <jefferdcondiza@gmail.com>
2025-03-31 10:29:08 -0500

    Integrate NATS microservices for payment processing and update environment configurations

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -7,6 +7,7 @@ interface EnvVars {
   STRIPE_SUCCESS_URL: string;
   STRIPE_CANCEL_URL: string;
   STRIPE_ENDPOINTSECRET: string;
+  NTAS_SERVERS: string[];
 }
 
 const envsSchema = joi
@@ -16,10 +17,15 @@ const envsSchema = joi
     STRIPE_SUCCESS_URL: joi.string().required(),
     STRIPE_CANCEL_URL: joi.string().required(),
     STRIPE_ENDPOINTSECRET: joi.string().required(),
+    NTAS_SERVERS: joi.array().items(joi.string()).required(),
   })
   .unknown(true);
 
-const { error, value } = envsSchema.validate(process.env);
+const { error, value } = envsSchema.validate({
+  ...process.env,
+  NTAS_SERVERS: process.env.NTAS_SERVERS?.split(','),
+});
+
 if (error) {
   throw new Error(`Config validation error ${error.message}`);
 }
@@ -28,8 +34,11 @@ const envVars: EnvVars = value;
 
 export const envs = {
   port: envVars.PORT,
+
   stripeSecret: envVars.STRIPE_SECRET,
   stripeSuccessUrl: envVars.STRIPE_SUCCESS_URL,
-  stripeCancelUrl: envVars.STRIPE_CANCEL_URL, 
+  stripeCancelUrl: envVars.STRIPE_CANCEL_URL,
   stripeEndpointSecret: envVars.STRIPE_ENDPOINTSECRET,
+
+  natsServers: envVars.NTAS_SERVERS,
 };
